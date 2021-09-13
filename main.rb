dd15d9ad348e131385e2e54aedef51588f66401f
Michael <michael.dburke56@gmail.com>
2021-09-13 15:15:43 -0400

    Build Player class

---
+++ main.rb
@@ -0,0 +1,13 @@
+require './player'
+
+player1 = Player.new("Mike")
+player2 = Player.new("Brianna")
+
+
+puts "--- player 1 ---"
+puts player1.lives
+puts player1.name
+
+puts "--- player 2 ---"
+puts player2.lives
+puts player2.name
